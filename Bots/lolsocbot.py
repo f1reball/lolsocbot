e1454f854b00ec077b167675f17ad8a786103631
Jackson Booth <jacksontbooth@icloud.com>
2019-04-24 04:21:27 +1000

    fixed to new revision of discord.py

--- Bots/lolsocbot.py
+++ Bots/lolsocbot.py
@@ -1,22 +1,20 @@
-import discord
+from discord.ext import commands
+
 TOKEN = "NTcwMjgwMjMwMTg3NTY1MDU2.XL86Qw.GyFP-zN1cxco28qj8HexiqvU_lw"
+bot = commands.Bot(command_prefix = "!")
 
-client = discord.Client()
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello {}".format(ctx.author.mention))
 
-@client.event
-async def on_message(message):
-    #hello
-    if message.content.startswith("!hello"):
-        msg = "Hello {0.author.mention}".format(message)
-        if "Exec" or "Subcom" in (role.name for role in message.author.roles):
-            print("Yay")
-        await message.channel.send(msg)
-
-    #FF
-    if message.content == "!FF":
-        msg = "Welcome to UNSW LoLSoc's Week {} Friendly Friday, an event PROUDLY sponsored by Arc Clubs"
-
+@bot.command()
+async def FF(ctx, week):
+    if "Executive" in (role.name for role in ctx.author.roles):
+        await ctx.send("Welcome to UNSW LoLSoc's Week {} Friendly Friday, an event PROUDLY sponsored by Arc Clubs".format(week))
+    elif "Subcommittee" in (role.name for role in ctx.author.roles):
+        await ctx.send("Welcome to UNSW LoLSoc's Week {} Friendly Friday, an event PROUDLY sponsored by Arc Clubs".format(week))
+    else:
+        pass
 
 
-
-client.run(TOKEN)
+bot.run(TOKEN)
